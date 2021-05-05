c67f1b3a1423c8fc8462f2052cdbd6a67c032208
Osman Koç <info@osmkoc.com>
2021-05-06 00:57:13 +0300

    Catalog / added mongodb models

---
+++ Services/Catalog/NET5Academy.Services.Catalog/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NET5Academy.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Services/Catalog/NET5Academy.Services.Catalog/Models/Feature.cs
@@ -0,0 +1,7 @@
+﻿namespace NET5Academy.Services.Catalog.Models
+{
+    public class Feature
+    {
+        public int Duration { get; set; }
+    }
+}
