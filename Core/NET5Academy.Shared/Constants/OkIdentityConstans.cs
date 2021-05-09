184a7357c344abbe2b9a48f3d7791d5ef65af8b7
Osman Koç <info@osmkoc.com>
2021-05-09 03:02:35 +0300

    Added IdentityServer constants (Shared) & Modified config (IdentityServer)

---
+++ Core/NET5Academy.Shared/Constants/OkIdentityConstans.cs
@@ -0,0 +1,23 @@
+﻿namespace NET5Academy.Shared.Constants
+{
+    public class OkIdentityConstans
+    {
+        public static class ScopeName
+        {
+            public const string CatalogAPI = "catalog_fullpermission";
+            public const string PhotoStockAPI = "photostock_fullpermission";
+        }
+        public static class ScopeDisplay
+        {
+            public const string IdentityAPI = "IdentityServer API";
+            public const string CatalogAPI = "Catalog API full permission";
+            public const string PhotoStockAPI = "PhotoStock API full permission";
+        }
+
+        public static class ResourceName
+        {
+            public const string CatalogAPI = "resource_catalog";
+            public const string PhotoStockAPI = "resource_photostock";
+        }
+    }
+}
--- IdentityServer/NET5Academy.IdentityServer/Startup.cs
+++ IdentityServer/NET5Academy.IdentityServer/Startup.cs
@@ -42,6 +42,7 @@ namespace NET5Academy.IdentityServer
                 options.EmitStaticAudienceClaim = true;
             })
                 .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiResources(Config.ApiResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
                 .AddAspNetIdentity<ApplicationUser>();
