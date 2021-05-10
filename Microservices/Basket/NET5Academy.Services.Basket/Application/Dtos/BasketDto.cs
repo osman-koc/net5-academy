b75464233a17a7cc49b07128aa4b63fa7c7cc60a
Osman Koç <info@osmkoc.com>
2021-05-11 00:33:59 +0300

    Basket / Added BasketDto & BasketItemDto

---
+++ Microservices/Basket/NET5Academy.Services.Basket/Application/Dtos/BasketDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NET5Academy.Services.Basket.Application.Dtos
+{
+    public class BasketDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public decimal TotalPrice { get => Items.Sum(x => x.Price * x.Quantity); }
+        public List<BasketItemDto> Items { get; set; }
+
+        public BasketDto()
+        {
+            Items = new List<BasketItemDto>();
+        }
+    }
+}
