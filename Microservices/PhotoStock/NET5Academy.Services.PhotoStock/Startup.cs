441aba9d8f313dfa05591d61885d9ad4706bf9ca
Osman Koç <info@osmkoc.com>
2021-05-10 23:51:07 +0300

    PhotoStock / added identity authentication to Startup

--- Microservices/PhotoStock/NET5Academy.Services.PhotoStock/Startup.cs
+++ Microservices/PhotoStock/NET5Academy.Services.PhotoStock/Startup.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using NET5Academy.Services.PhotoStock.Application.Services;
+using NET5Academy.Shared.Constants;
 
 namespace NET5Academy.Services.PhotoStock
 {
@@ -18,11 +21,23 @@ namespace NET5Academy.Services.PhotoStock
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(new AuthorizeFilter());
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "NET5Academy.Services.PhotoStock", Version = "v1" });
             });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = Configuration["IdentityServerUri"];
+                    options.Audience = OkIdentityConstans.ResourceName.PhotoStockAPI;
+                    options.RequireHttpsMetadata = false;
+                });
 
             services.AddScoped<IPhotoService, PhotoService>();
         }
@@ -41,6 +56,7 @@ namespace NET5Academy.Services.PhotoStock
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
