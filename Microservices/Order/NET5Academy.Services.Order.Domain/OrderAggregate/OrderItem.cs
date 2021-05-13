a0844c811af00608de123210d6830c20760cc304
Osman Koç <info@osmkoc.com>
2021-05-14 01:59:30 +0300

    Order / Order and OrderItem models added to Domain layer

---
+++ Microservices/Order/NET5Academy.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿namespace NET5Academy.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem
+    {
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string ImageUrl { get; private set; }
+        public decimal Price { get; private set; }
+        public int Quantity { get; private set; }
+
+        public OrderItem(string productId, string productName, string imageUrl, decimal price, int quantity)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            ImageUrl = imageUrl;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public void Update(string productName, string imageUrl, decimal price, int quantity)
+        {
+            ProductName = productName;
+            ImageUrl = imageUrl;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
