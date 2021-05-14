432c348dd0705f1c93a04aaeeaf492db09658631
Osman Koç <info@osmkoc.com>
2021-05-14 17:33:52 +0300

    Order / Added ObjectMapper and OkAutoMapping to Application layer

---
+++ Microservices/Order/NET5Academy.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+
+namespace NET5Academy.Services.Order.Application.Mapping
+{
+    public class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> _lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(options =>
+            {
+                options.AddProfile<OkAutoMapping>();
+            });
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => _lazy.Value;
+    }
+}
---
+++ Microservices/Order/NET5Academy.Services.Order.Application/Mapping/OkAutoMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NET5Academy.Services.Order.Application.Dtos;
+
+namespace NET5Academy.Services.Order.Application.Mapping
+{
+    public class OkAutoMapping : Profile
+    {
+        public OkAutoMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<Domain.OrderAggregate.OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Domain.OrderAggregate.Address, AddressDto>().ReverseMap();
+        }
+    }
+}
