7daa26d2caf737b286c23902a0c5446cba0efcd2
Osman Koç <info@osmkoc.com>
2021-05-14 17:42:48 +0300

    Order / Added GetOrdersByUserIdQuery to Application layer

---
+++ Microservices/Order/NET5Academy.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using NET5Academy.Services.Order.Application.Dtos;
+using NET5Academy.Shared.Models;
+using System.Collections.Generic;
+
+namespace NET5Academy.Services.Order.Application.Queries
+{
+    public class GetOrdersByUserIdQuery : IRequest<OkResponse<IEnumerable<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
