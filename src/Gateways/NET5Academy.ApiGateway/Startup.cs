2eb08250ea24696c103c8ec4f45e9ec90bd6f045
Osman Koç <info@osmkoc.com>
2021-05-15 18:58:36 +0300

    ApiGateway / Configure startup

--- src/Gateways/NET5Academy.ApiGateway/Startup.cs
+++ src/Gateways/NET5Academy.ApiGateway/Startup.cs
@@ -1,12 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace NET5Academy.ApiGateway
 {
@@ -14,24 +11,17 @@ namespace NET5Academy.ApiGateway
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        async public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            await app.UseOcelot();
         }
     }
 }
