0ee7836dfde0592dada7dddc8e25a34edf3ad699
Osman Koç <info@osmkoc.com>
2021-05-17 00:03:55 +0300

    Web / Upload IdentityModel nuget package & Added IIdentityService interface

---
+++ src/Presentation/NET5Academy.Web/Models/Services/SignInModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NET5Academy.Web.Models.Services
+{
+    public class SignInModel
+    {
+        [Display(Name = "E-posta adresi")]
+        public string Email { get; set; }
+
+        [Display(Name = "Parola")]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni hatırla")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ src/Presentation/NET5Academy.Web/Services/IdentityServer/IIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using IdentityModel.Client;
+using NET5Academy.Shared.Models;
+using NET5Academy.Web.Models.Services;
+using System.Threading.Tasks;
+
+namespace NET5Academy.Web.Services
+{
+    public interface IIdentityService
+    {
+        Task<OkResponse<bool>> SignIn(SignInModel model);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+    }
+}
