60173a6d6865bd2e5ac521d08ec850001061c598
Osman Koç <info@osmkoc.com>
2022-02-08 01:29:47 +0300

    UI / Added identityService RevokeRefreshToken method to logout page

--- src/Presentation/NET5Academy.Web/Pages/Logout.cshtml.cs
+++ src/Presentation/NET5Academy.Web/Pages/Logout.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using NET5Academy.Web.Services;
 using System.Threading.Tasks;
 
 namespace NET5Academy.Web.Pages
@@ -9,9 +10,11 @@ namespace NET5Academy.Web.Pages
     public class LogoutModel : PageModel
     {
         private readonly ILogger<LogoutModel> _logger;
-        public LogoutModel(ILogger<LogoutModel> logger)
+        private readonly IIdentityService _identityService;
+        public LogoutModel(ILogger<LogoutModel> logger, IIdentityService identityService)
         {
             _logger = logger;
+            _identityService = identityService;
         }
 
         public async Task OnGet()
@@ -19,6 +22,7 @@ namespace NET5Academy.Web.Pages
             _logger.LogDebug("Logout - Deleting all cookies..");
 
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await _identityService.RevokeRefreshToken();
 
             //foreach (var item in Request.Cookies.Keys)
             //{
